14988b727b5cee654c0278f20e553e67fbd6887f
Arifurrahmanrifat29112002 <arifurrahmanrifat72@gmail.com>
2023-10-11 23:26:36 +0600

    DOM  EventListener

--- Dom.js
+++ Dom.js
@@ -71,36 +71,170 @@
 
 
 /**
- * 35.Image slider mini project
+ * 36.Image slider mini project
  */
 
-var ImageGallary = ["https://c4.wallpaperflare.com/wallpaper/968/1007/710/technology-programming-wallpaper-preview.jpg","https://c4.wallpaperflare.com/wallpaper/372/843/684/chip-connection-diagnosis-blur-wallpaper-preview.jpg","https://images.pexels.com/photos/52608/pexels-photo-52608.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"];
+// var ImageGallary = ["https://c4.wallpaperflare.com/wallpaper/968/1007/710/technology-programming-wallpaper-preview.jpg","https://c4.wallpaperflare.com/wallpaper/372/843/684/chip-connection-diagnosis-blur-wallpaper-preview.jpg","https://images.pexels.com/photos/52608/pexels-photo-52608.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"];
 
 
-//find image tag
-var image = document.querySelector("img");
-//count 
-var count = 0;
+// //find image tag
+// var image = document.querySelector("img");
+// //count 
+// var count = 0;
 
-function Next(){
-  count++;
-  if(count >= ImageGallary.length){
-    count = 0;
-    image.src = ImageGallary[count];
-  }else{
-    image.src = ImageGallary[count];
-  }
+// function Next(){
+//   count++;
+//   if(count >= ImageGallary.length){
+//     count = 0;
+//     image.src = ImageGallary[count];
+//   }else{
+//     image.src = ImageGallary[count];
+//   }
   
-}
+// }
 
-function Pre(){
-    count--;
-    if (count < 0) {
-        count = ImageGallary.length-1
-        image.src = ImageGallary[count];
-    }else{
-        image.src = ImageGallary[count];
-    }
+// function Pre(){
+//     count--;
+//     if (count < 0) {
+//         count = ImageGallary.length-1
+//         image.src = ImageGallary[count];
+//     }else{
+//         image.src = ImageGallary[count];
+//     }
 
     
-}
+// }
+
+
+/**
+ * 37.changing css style dynamically
+ * 1st create index
+ * 2nd create css file
+ * 3rd css file linking index file
+ * 4th create js
+ */
+
+// var para = document.querySelector("#paraId");
+// function addStyle(){
+//     para.classList.add("para-style");
+// }
+
+// function removeStyle(){
+//     para.classList.remove("para-style");
+// }
+
+
+/**
+ * 38.Event Listener
+ * 1st create index
+ * 2nd create css file
+ * 3rd css file linking index file
+ * 4th create js
+ */
+
+// var h1var = document.querySelector("h1");
+// h1var.addEventListener("mouseover",function(){
+//   h1var.classList.add("h1-style");
+// });
+
+// h1var.addEventListener("mouseout",function(){
+//     h1var.classList.remove("h1-style");
+// });
+
+
+
+/**
+ * 39.Event Listeners with multiple elements
+ */
+// var buttonsLen = document.querySelectorAll(".button").length;
+
+// for (var i =0; i < buttonsLen; i++) {
+
+//     document.querySelectorAll(".button")[i].addEventListener("click",function(){
+//         var text = this.innerHTML;
+//         document.querySelector("h1").innerHTML = text +" button is clicked";
+//     })
+
+// }
+
+/**
+ * 40.how to play audio in javascript
+ */
+
+// for (var i = 0; i < 3; i++) {
+//     document.querySelectorAll("button")[i].addEventListener('click',function(){
+//         var text = this.innerHTML;
+
+//         audioPlay(text);
+//     });
+// }
+
+// function audioPlay(text){
+//         switch(text){
+//             case 'a':
+//                 var audio = new Audio('audio_file.mp3');
+//                 audio.play();
+//                 break;
+    
+//             case 'b':
+//                 var audio = new Audio('audio_file.mp3');
+//                 audio.play();
+//                 break;
+    
+//             case 'c':
+//                 var audio = new Audio('audio_file.mp3');
+//                 audio.play();
+//                 break;
+//         }
+// }
+
+
+/**
+ * 41.How to add and remove animation
+ */
+// var buttonsLen = document.querySelectorAll("button").length;
+
+// for(var i = 0; i<buttonsLen;i++){
+
+//     document.querySelectorAll("button")[i].addEventListener('click',function(){
+//         var text = this.innerHTML;
+//         itemAnimation(text);
+//     })
+    
+// }
+
+// function itemAnimation(text){
+//     var selectButton = document.querySelector("."+text);
+//     selectButton.classList.add("button_animation");
+
+//     setTimeout(function(){
+//         selectButton.classList.remove("button_animation");
+//     },1000);
+// }
+
+
+/**
+ * 42.Keypress listener
+ */
+
+// for (var i =0; i < 3; i++) {
+//     document.querySelectorAll("button")[i].addEventListener("click",function(){
+//         var text = this.innerHTML;
+    
+//         itemAnimation(text);
+//     }) 
+// }
+
+// document.addEventListener("keypress",function(event){
+//     var evenT = event.key;
+//      itemAnimation(evenT);
+// })
+
+// function itemAnimation(text){
+//     var selectButton = document.querySelector("."+text);
+//     selectButton.classList.add("button_animation");
+
+//     setTimeout(function(){
+//         selectButton.classList.remove("button_animation");
+//     },1000);
+// }
