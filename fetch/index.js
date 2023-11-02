7d650fe56dd5c70f29623e557a8f1bd49e952ffb
Arifurrahmanrifat29112002 <arifurrahmanrifat72@gmail.com>
2023-11-02 21:29:28 +0600

    fetch api

---
+++ fetch/index.js
@@ -0,0 +1,83 @@
+/**
+ *  calling api from javascript | fetch api
+ */
+
+
+const makeRequest = (url,Data) =>{
+    fetch(url,Data)
+    .then((res) =>{
+        if (!res.ok) {
+            throw new Error(`Error:${res.status}`)
+        }
+       return res.json() 
+    })
+    .then((res) => console.log(res))
+    .catch((error) => console.log(error))
+}
+
+//get all data
+    const getData = () => {
+        makeRequest('https://jsonplaceholder.typicode.com/posts')
+    }
+    getData();
+
+//create a new data 
+    const sendData = () => {
+        makeRequest('https://jsonplaceholder.typicode.com/posts',{
+            method: 'POST',
+
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+
+            body: JSON.stringify({
+              title: 'Hello world',
+              body: 'hello programmer',
+              userId: 1,
+            }),
+        })
+    }
+    sendData();
+//update data
+    const updateData = () =>{
+        makeRequest('https://jsonplaceholder.typicode.com/posts/1',{
+            method: 'PUT',
+            body: JSON.stringify({
+                title: 'hello world',
+                body: 'hello world',
+                userId: 1,
+              }),
+              headers: {
+                'Content-type':'application/json'
+              },
+        })
+    }
+    updateData()
+
+//update single data 
+    const updateSingleData = () =>{
+        makeRequest('https://jsonplaceholder.typicode.com/posts/1',{
+            method: 'PATCH',
+            body: JSON.stringify({
+                title: 'hello programmers',
+              }),
+              headers: {
+                'Content-type':'application/json'
+              },
+        })
+    }
+    updateSingleData();
+
+//delete data 
+    const deleteData = () =>{
+        makeRequest('https://jsonplaceholder.typicode.com/posts/1',{
+            method: 'DELETE',
+            body: JSON.stringify({
+                title: 'hello programmers',
+              }),
+              headers: {
+                'Content-type':'application/json'
+              },
+        })
+    }
+    deleteData();
